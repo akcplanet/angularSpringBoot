a9ef7eb0c49bd38e1b2092e4ce32fe893ccb2136
Alexis <amitchaudhary.it@gmail.com>
2017-08-20 11:19:06 +0530

    Add files via upload

---
+++ failtest/src/alexis/alexis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alexis-root',
+  templateUrl: './alexis.component.html',
+  styleUrls: ['./alexis.component.css']
+})
+export class AlexisComponent {
+  title = 'Alexis';
+}
---
+++ failtest/src/alexis/alexis.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AlexisComponent } from './alexis.component';
+
+@NgModule({
+  declarations: [
+    AlexisComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AlexisComponent]
+})
+export class AlexisModule { }
